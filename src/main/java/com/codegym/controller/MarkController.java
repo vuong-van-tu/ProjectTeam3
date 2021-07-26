0ba36063e58fdd2a3bb02b8c6501dbf180d344e9
vuongvantu <vuongvantu2001x@gmail.com>
2021-07-26 20:47:40 +0700

    view

--- src/main/java/com/codegym/controller/MarkController.java
+++ src/main/java/com/codegym/controller/MarkController.java
@@ -1,4 +1,7 @@
 package com.codegym.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MarkController {
 }
--- src/main/java/com/codegym/controller/UserController.java
+++ src/main/java/com/codegym/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.codegym.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("users")
 public class UserController {
+    @GetMapping
+    public String showform(){
+        return "/index";
+    }
 }
