24eb34ca355cd06c2c37d45e1a1463e89d8052ed
vuongvantu <vuongvantu2001x@gmail.com>
2021-07-27 16:26:11 +0700

    view1

--- src/main/java/com/codegym/controller/DiaryClassController.java
+++ src/main/java/com/codegym/controller/DiaryClassController.java
@@ -1,14 +1,9 @@
 package com.codegym.controller;
 
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/diaryClass")
-=======
-
-@Controller
->>>>>>> eda6a061223fa8434b8dff5338fc795f72f13452
 public class DiaryClassController {
 }
--- src/main/java/com/codegym/controller/SubjectController.java
+++ src/main/java/com/codegym/controller/SubjectController.java
@@ -4,10 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-<<<<<<< HEAD
 @RequestMapping("/subjects")
-=======
-@RequestMapping("")
->>>>>>> eda6a061223fa8434b8dff5338fc795f72f13452
 public class SubjectController {
 }
